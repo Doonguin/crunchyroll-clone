6b2d97eb3d32e8a59c91904b3840412fd5e21b13
Doonguin <penguinicesp@gmail.com>
2023-01-16 12:24:38 +0100

    Crunchyroll header

---
+++ src/components/crunchytop.vue
@@ -0,0 +1,31 @@
+<template>
+    <img src="/crunchyroll_header.png" alt="crunchy" />
+
+    <a>
+        Browse
+        <img src="/arrow_down.png" alt="" />
+    </a>
+    <a>
+        Manga
+    </a>
+    <a>
+        Games
+    </a>
+    <a>
+        Store
+    </a>
+    <a>
+        News
+        <img src="/arrow_down.png" alt="" />
+    </a>
+
+    <a pos="end">
+        <img src="/search.png" alt="" />
+    </a>
+    <a img="beeg">
+        <img src="/bookmark.png" alt="" />
+    </a>
+    <a img="beeg">
+        <img src="/user.png" alt="" />
+    </a>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import './assets/header.css'
 import './assets/main.css'
 
 createApp(App).mount('#app')
